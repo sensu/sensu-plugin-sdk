eae7989456a4be0d1604e39ac41801630effbf4c
Cyril Cressent <cyril@cressent.org>
2020-02-04 09:36:07 -0800

    Support for Check status code
    Signed-off-by: Cyril Cressent <cyril@cressent.org>

--- sensu/gocheck.go
+++ sensu/gocheck.go
@@ -7,14 +7,22 @@ import (
 	"github.com/sensu/sensu-go/types"
 )
 
+const (
+	CheckStateOK       = 0
+	CheckStateWarning  = 1
+	CheckStateCritical = 2
+	CheckStateUnknown  = 3
+)
+
 type GoCheck struct {
 	basePlugin
-	validationFunction func(event *types.Event) error
-	executeFunction    func(event *types.Event) error
+	validationFunction func(event *types.Event) (int, error)
+	executeFunction    func(event *types.Event) (int, error)
 }
 
 func NewGoCheck(config *PluginConfig, options []*PluginConfigOption,
-	validationFunction func(event *types.Event) error, executeFunction func(event *types.Event) error) *GoCheck {
+	validationFunction func(event *types.Event) (int, error),
+	executeFunction func(event *types.Event) (int, error)) *GoCheck {
 	check := &GoCheck{
 		basePlugin: basePlugin{
 			config:                 config,
@@ -38,16 +46,16 @@ func NewGoCheck(config *PluginConfig, options []*PluginConfigOption,
 // Executes the check
 func (goCheck *GoCheck) goCheckWorkflow(_ []string) (int, error) {
 	// Validate input using validateFunction
-	err := goCheck.validationFunction(goCheck.sensuEvent)
+	status, err := goCheck.validationFunction(goCheck.sensuEvent)
 	if err != nil {
-		return 1, fmt.Errorf("error validating input: %s", err)
+		return status, fmt.Errorf("error validating input: %s", err)
 	}
 
-	// Execute handler logic using executeFunction
-	err = goCheck.executeFunction(goCheck.sensuEvent)
+	// Execute check logic using executeFunction
+	status, err = goCheck.executeFunction(goCheck.sensuEvent)
 	if err != nil {
-		return 1, fmt.Errorf("error executing handler: %s", err)
+		return status, fmt.Errorf("error executing check: %s", err)
 	}
 
-	return 0, nil
+	return status, nil
 }
