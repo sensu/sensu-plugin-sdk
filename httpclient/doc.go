2ff9b269c26156d4ca3d3327a15b6e91db77d123
Eric Chlebek <eric@sensu.io>
2020-02-05 02:19:58 -0800

    Add Sensu HTTP APi client
    Signed-off-by: Eric Chlebek <eric@sensu.io>

---
+++ httpclient/doc.go
@@ -0,0 +1,4 @@
+// Package httpclient aims to be a convenient tool for accessing the Sensu API
+// from within Go. A generic client for the core API is provided, along with
+// a generalized way to represent Sensu resources.
+package httpclient
