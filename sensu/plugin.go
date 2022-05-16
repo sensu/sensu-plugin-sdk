41d0e03f8946bcefbf9b36682ee50789fb14939a
Eric Chlebek <eric@sensu.io>
2022-05-16 14:38:34 -0700

    A quick test
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- sensu/plugin.go
+++ sensu/plugin.go
@@ -357,7 +357,10 @@ func (p *PluginConfigOption[T]) SetupFlag(cmd *cobra.Command) error {
 		case reflect.Float64:
 			cmd.Flags().Float64VarP(ptr.(*float64), p.Argument, p.Shorthand, viper.GetFloat64(p.Argument), p.Usage)
 		case reflect.String:
-			cmd.Flags().StringVarP(ptr.(*string), p.Argument, p.Shorthand, viper.GetString(p.Argument), p.Usage)
+			newType := reflect.TypeOf("")
+			rvalue = rvalue.Convert(newType)
+			ptr := rvalue.Addr().Interface().(*string)
+			cmd.Flags().StringVarP(ptr, p.Argument, p.Shorthand, viper.GetString(p.Argument), p.Usage)
 		}
 		return fmt.Errorf("setup flag: %s: unknown value type", p.Argument)
 	}
